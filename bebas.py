5ee80ff9e570237a66397de81d7f3db4c884bb87
BilkisShakila <bilkisshakilaa@gmail.com>
2025-10-07 10:41:24 +0700

    first commit

---
+++ bebas.py
@@ -0,0 +1,9 @@
+print ("Hello World") 
+
+
+nama = input("masukkan nama kalian")
+umur = int (input("masukkan umur kalian"))
+
+print ("nama saya " + nama )
+print ("umur saya " + str(umur))
+
---
+++ pertemuan 2/prak1.py
@@ -0,0 +1,13 @@
+percentage = float(input("masukkan persentase nilai siswa:"))
+
+if percentage >= 90:
+        print("Excellent perfomance")
+elif percentage >= 80:
+        print("Very Good perfomance")
+elif percentage >= 70:
+    print("Good performance")   
+elif percentage >= 60:
+    print("Satisfactory performance")
+else:
+    print("Needs Improvement")
+    
