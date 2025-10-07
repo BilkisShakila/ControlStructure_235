d4a30e0a90c550fe4bbc06aa8d1c309b7a81a8cb
BilkisShakila <bilkisshakilaa@gmail.com>
2025-10-07 10:51:31 +0700

    Menambahkan program untuk menentukan angka terbesar dari tiga input

--- pertemuan 2/prak2.py
+++ pertemuan 2/prak2.py
@@ -1,6 +1,7 @@
 a = float (input("Masukkan angka pertama: "))
 b = float (input("Masukkan angka kedua: "))
-c = float (input("Masukkan angka ketiga: "))
+c = float (input("Masukkan angka ketiga: ")) 
+
 
 if a >= b and a >= c:
     largest = a
